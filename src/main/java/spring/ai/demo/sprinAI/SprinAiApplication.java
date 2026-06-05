94bcfba0af67aad7100923f995fee9774fe84b41
Archit Khankriyal <karchit@gkblabs.com>
2026-06-05 16:21:34 +0530

    initial commit

---
+++ src/main/java/spring/ai/demo/sprinAI/SprinAiApplication.java
@@ -0,0 +1,13 @@
+package spring.ai.demo.sprinAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinAiApplication.class, args);
+	}
+
+}
